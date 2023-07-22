02aae0c902bd088a24b1737795e25ec80f2df185
kloir-z <kosuke.otsu@gmail.com>
2023-07-22 22:44:31 +0900

    On branch main  Your branch is up to date with 'origin/main'.
     Changes to be committed:
    	new file:   config.json
    	modified:   image_viewer.py

--- image_viewer.py
+++ image_viewer.py
@@ -1,10 +1,12 @@
 import os
 import sys
+import json
 from datetime import datetime
-from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QVBoxLayout, QSizePolicy  
+from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QVBoxLayout, QSizePolicy ,QMenu, QAction
 from PyQt5.QtGui import QPixmap, QImage
 from PyQt5.QtCore import Qt
 from PIL import Image
+from collections import OrderedDict
 
 class ResizableLabel(QLabel):
     def __init__(self, *args, **kwargs):
@@ -18,23 +20,41 @@ class ImageViewer(QWidget):
 
         self.index = 0
         self.images = []
+        self.config_path = 'config.json'
+        self.supported_extensions = ['.png', '.xpm', '.gif', '.bmp', '.jpg']
+
+        if os.path.exists(self.config_path):
+            with open(self.config_path, 'r') as f:
+                config = json.load(f)
+            history = list(config.get('history', {}).keys())
+            position = config.get('position', [0, 0])
+            size = config.get('size', [800, 800])
+        else:
+            history = []
+            position = [0, 0]
+            size = [800, 800]
+
+        self.history = [x for n, x in enumerate(history[::-1]) if x not in history[::-1][n+1:]]
+        self.history = OrderedDict.fromkeys(self.history)
 
         self.layout = QVBoxLayout()
         self.setLayout(self.layout)
 
         self.label = ResizableLabel()
         self.layout.addWidget(self.label)
+        
+        self.setContextMenuPolicy(Qt.CustomContextMenu)
+        self.customContextMenuRequested.connect(self.show_context_menu)
 
         self.setAcceptDrops(True)
 
         screen_size = QApplication.instance().desktop().screenGeometry()
         self.setMaximumSize(screen_size.width(), screen_size.height())
 
-        self.resize(800, 800)
-        self.move(0, 0)
+        self.resize(*size)
+        self.move(*position)
 
         self.original_pixmap = None
- 
         self.is_loading = False
 
     def mousePressEvent(self, event):
@@ -44,24 +64,19 @@ class ImageViewer(QWidget):
         elif x > self.width() * 0.75:
             self.show_next_image()
 
+    def show_next_image(self):
+        self.move_index(1)
+
     def show_prev_image(self):
+        self.move_index(-1)
+
+    def move_index(self, delta):
         if not self.images or self.is_loading:
             return
-        self.index -= 1
-        if self.index < 0:
-            self.index = len(self.images) - 1
+        self.index += delta
+        self.index %= len(self.images)
         self.update_image()
         self.label.setPixmap(self.get_scaled_pixmap()) 
-        self.is_loading = False
-
-    def show_next_image(self):
-        if not self.images or self.is_loading:
-            return
-        self.index += 1
-        if self.index >= len(self.images):
-            self.index = 0
-        self.update_image()
-        self.label.setPixmap(self.get_scaled_pixmap())
         self.is_loading = False
 
     def keyPressEvent(self, event):
@@ -86,7 +101,7 @@ class ImageViewer(QWidget):
             weekday_conversion = {"Mon": "月", "Tue": "火", "Wed": "水", "Thu": "木", "Fri": "金", "Sat": "土", "Sun": "日"}
             for eng, jp in weekday_conversion.items():
                 formatted_time = formatted_time.replace(eng, jp)
-            self.setWindowTitle(f"{os.path.basename(image_path)} - {formatted_time}")
+            self.setWindowTitle(f"{os.path.basename(image_path)} - {formatted_time} - {self.index + 1}/{len(self.images)}")
 
             try:
                 exif = image._getexif()
@@ -114,8 +129,7 @@ class ImageViewer(QWidget):
 
             data = image.tobytes("raw", "RGB")
             qimage = QImage(data, image.size[0], image.size[1], image.size[0]*3, QImage.Format_RGB888)
-            pixmap = QPixmap.fromImage(qimage)
- 
+
         self.original_pixmap = QPixmap.fromImage(qimage)
         self.label.setPixmap(self.original_pixmap)
         self.is_loading = False
@@ -136,6 +150,16 @@ class ImageViewer(QWidget):
         if event.mimeData().hasUrls():
             event.acceptProposedAction()
 
+    def closeEvent(self, event):
+        config = {
+            'history': self.history,
+            'position': [self.x(), self.y()],
+            'size': [self.width(), self.height()],
+        }
+        with open(self.config_path, 'w') as f:
+            json.dump(config, f)
+        event.accept()
+
     def dropEvent(self, event):
         self.images = []
         dropped_file_path = None
@@ -144,20 +168,47 @@ class ImageViewer(QWidget):
             if os.path.isdir(path):
                 dir_path = path
             else:
-                if path.lower().endswith(('.png', '.xpm', '.gif', '.bmp', '.jpg')):
+                if path.lower().endswith(tuple(self.supported_extensions)):
                     dropped_file_path = os.path.normpath(path)
                 dir_path = os.path.dirname(path)
             files = sorted(os.listdir(dir_path))
             for file in files:
-                if file.lower().endswith(('.png', '.xpm', '.gif', '.bmp', '.jpg')):
+                if file.lower().endswith(tuple(self.supported_extensions)):
                     self.images.append(os.path.normpath(os.path.join(dir_path, file)))
         if dropped_file_path:
             self.index = self.images.index(dropped_file_path)
         else:
             self.index = 0
+
         if self.images:
+            self.history[dir_path] = None
+            self.history = OrderedDict(list(self.history.items())[-20:])
+
             self.update_image()
-            self.label.setPixmap(self.get_scaled_pixmap()) 
+            self.label.setPixmap(self.get_scaled_pixmap())
+
+    def show_context_menu(self, position):
+        context_menu = QMenu(self)
+
+        for dir_path in list(self.history.keys())[::-1]:
+            if os.path.exists(dir_path):
+                action = QAction(dir_path, self)
+                action.triggered.connect(lambda _, d=dir_path: self.load_images_from_dir(d))
+                context_menu.addAction(action)
+
+        context_menu.exec_(self.mapToGlobal(position))
+
+    def load_images_from_dir(self, dir_path):
+        self.images = []
+        files = sorted(os.listdir(dir_path))
+        for file in files:
+            if file.lower().endswith(('.png', '.xpm', '.gif', '.bmp', '.jpg')):
+                self.images.append(os.path.normpath(os.path.join(dir_path, file)))
+
+        if self.images:
+            self.index = 0
+            self.update_image()
+            self.label.setPixmap(self.get_scaled_pixmap())
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
